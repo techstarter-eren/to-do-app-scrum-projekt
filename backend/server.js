e218a0185308ab265654343741b79beb7d65d94c
eren@techstarter <eren.tosyali@techstarter.de>
2025-03-05 12:54:58 +0100

    datenbank erstellt, post request zum Item hinzufügen geschrieben und getestet

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 //TODO: Verbinde eine Datenbank dazu
+
+const db = new sqlite3.Database('./tasks.db');
+
+app.use(bodyParser.json());     // Middleware
+
+
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
+
 
 //TODO: Schreibe requests/responses
 
@@ -14,6 +23,12 @@ app.get('/', (req, res) => {
 
 app.get('/ralf', (req, res) => {
     res.send('vielen Dank Ralf');
+});
+
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
+        res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
+    });
 });
 
 
