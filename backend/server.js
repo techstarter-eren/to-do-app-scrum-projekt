f2d1962abfd258c070c2b98371240b40b2a40ce4
eren@techstarter <eren.tosyali@techstarter.de>
2025-03-06 10:10:09 +0100

    liste_abrufen GET Request hinzugefügt

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 const db = new sqlite3.Database('./tasks.db');
 
-app.use(bodyParser.json());     // Middleware
+app.use(bodyParser.json());     // Middleware (wie ein Übersetzer)
 
 
 db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
@@ -25,11 +25,22 @@ app.get('/ralf', (req, res) => {
     res.send('vielen Dank Ralf');
 });
 
+// Wenn ein neues Item hinzugefügt werden soll, soll NodeJS Server diesen Request so behandeln:
 app.post('/add', (req, res) => {
     db.run('INSERT INTO tasks (title) VALUES (?)', [req.body.title], function () {
         res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"});
     });
 });
+
+
+// Liste mir alle existierende Items
+// hier sollte nur alle Items als JSON im Response geschrieben werden
+app.get('/liste_abrufen', (req, res) => {
+    db.all('SELECT * FROM tasks', function (err, rows){
+        res.json(rows);
+    })
+});
+
 
 
 app.listen(3050, "localhost", () => {
