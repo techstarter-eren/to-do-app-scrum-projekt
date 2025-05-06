2f490067ce13e98c3ea14f0c6be940d77133e393
Chris | R0tBart | techstarter <christoph.von.hagen@tn.techstarter.de>
2025-05-06 08:52:41 +0200

    alles gut

--- frontend/test/chris.test.jsx
+++ frontend/test/chris.test.jsx
@@ -1,11 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import React from 'react';
+<<<<<<< Updated upstream
 import YourComponent from '../src/App'; // Replace with the actual path to your component
 
 describe('YourComponent', () => {
     test('renders input field with placeholder "Neue Kategorie..."', () => {
         render(<YourComponent />);
+=======
+import App from '../src/App'; // Replace with the actual path to your component
+
+describe('YourComponent', () => {
+    test('renders input field with placeholder "Neue Kategorie..."', () => {
+        render(<App />);
+>>>>>>> Stashed changes
         const inputElement = screen.getByPlaceholderText('Neue Kategorie...');
         expect(inputElement).toBeInTheDocument();
     });
